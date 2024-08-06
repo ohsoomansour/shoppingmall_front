50af4cc2590a9294b873ece4616ef9f15c08b044
osooman <ceoosm@naver.com>
2024-08-06 22:03:10 +0900

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to MVNO"/>
+    <post-list post-title="게시판 리스트"> </post-list>
+ </div>
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import PostList from './components/PostList.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    PostList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    <h1>{{ post_title }}</h1>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'PostList',
+        props:{
+            post_title: String,
+        }
+    }
+
+</script>
